df1b6138980ba353141b3ab041d419901225fd01
Kai Daniel Gonzalez <gkai70264@gmail.com>
2022-08-21 10:24:20 -0400

    add portage pcks

--- src/main.d
+++ src/main.d
@@ -4,6 +4,12 @@ import std.path;
 import std.conv;
 import core.cpuid;
 import std.algorithm;
+import std.file;
+int amountPacks() {
+  int len = 0;
+  foreach (string f ; dirEntries("/tmp/portage", SpanMode.depth)) { len += 1; }
+  return len;
+}
 
 void main() {
   writefln("
@@ -15,12 +21,12 @@ void main() {
 | | |,x   ,   | |      \033[1mCPU Cores\033[0m: %u
 | | |/    | ==| |      
 | |    /] ,   | |      \033[1mUser Directory\033[0m: %s
-| |   [/ ()   | |      
+| |   [/ ()   | |      \033[1mPortage packages installed\033[0m: %i 
 | |       |   | |      
 | |       |   | |      
 | |       |   | |      
 | |      ,'   | |      
 | |   ,'      | |      
 |_|,'_________|_|      
-", processor(), coresPerCPU(), expandTilde("~"));
+", processor(), coresPerCPU(), expandTilde("~"), amountPacks());
 }
